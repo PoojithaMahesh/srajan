9b9ed1fb4e65f1820bc6b7c1813521fb5fb2314c
unknown <poojithadarshan2131@gmail.com>
2023-09-01 16:06:48 +0530

    yes

---
+++ src/main/java/car1/Car.java
@@ -0,0 +1,21 @@
+package car1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class Car extends GenericServlet{
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+	String carName=req.getParameter("carName");
+	PrintWriter printWriter=res.getWriter();
+	printWriter.print("YOUR CAR NAME IS  "+carName);
+		
+	}
+
+}
